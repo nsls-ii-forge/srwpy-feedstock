b55ef8eae530406ed9b26d29b205b89e0c3aee7c
Maksim Rakitin <mrakitin@bnl.gov>
2020-09-02 00:48:51 -0400

    [skip ci] Remove unnecessary comments from build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,9 +5,5 @@ cd core/
 make all
 cd $idir
 
-# TODO: implement in the recipe for OSX later.
-# pip install delocate
-# delocate-listdeps dist/*.whl && delocate-wheel -v dist/*.whl
-
 ${PYTHON} setup.py build_ext --inplace
 ${PYTHON} -m pip install . --no-deps -vv
