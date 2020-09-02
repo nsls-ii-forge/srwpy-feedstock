a64ae129892e8f8d18d56fcb8eeb6a0643a51832
Maksim Rakitin <mrakitin@bnl.gov>
2020-09-01 23:14:52 -0400

    Try to tweak bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,4 @@
-msbuild core\\vc\\SRWLIB.vcxproj /maxcpucount:%NUMBER_OF_PROCESSORS% -property:Configuration=\"Release\";Platform=\"x64\""
+msbuild core\\vc\\SRWLIB.vcxproj /maxcpucount:%NUMBER_OF_PROCESSORS% -property:Configuration="Release";Platform="x64"
 
 %PYTHON% setup.py build_ext --inplace
 %PYTHON% -m pip install . --no-deps -vv
